0ee0492dfdff9b04367554f2ad4f11558fbb9f74
Andrew <supracharger@yahoo.com>
2025-04-20 16:58:59 -0600

    For a single movie return the single movie.

--- server.js
+++ server.js
@@ -188,7 +188,8 @@ router.route('/movies/:movieId')
         ]);
       if (!mov)
         return res.status(404).json({success: false, message: 'Unable to find movie.'});
-      return res.status(200).json({movie: mov, success: true});
+      mov = mov.json();
+      return res.status(200).json({...mov[0], success: true});
     })
     .put(authJwtController.isAuthenticated, async (req, res) => {
       var obj = {};
